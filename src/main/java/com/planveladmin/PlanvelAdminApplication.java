da2d411a2c5e9f65357bf9e38118192accc4d8b1
1seyoung <akdlrnal0705@gmail.com>
2025-05-11 13:30:23 +0900

    chore : Init

---
+++ src/main/java/com/planveladmin/PlanvelAdminApplication.java
@@ -0,0 +1,13 @@
+package com.planveladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanvelAdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlanvelAdminApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/planveladmin/PlanvelAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planveladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanvelAdminApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
